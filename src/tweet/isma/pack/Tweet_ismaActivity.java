6ff364acb065b6a74bb2b7cca2ab7bef720f4035
Noémie Lehuby <noemie.lehuby@centrale-marseille.fr>
2012-06-01 22:21:52 +0200

    Ceci est l'application de test proposée par Eclipse. Un bon début somme toute ;)

---
+++ src/tweet/isma/pack/Tweet_ismaActivity.java
@@ -0,0 +1,14 @@
+package tweet.isma.pack;
+
+import t.namespace.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tweet_ismaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
